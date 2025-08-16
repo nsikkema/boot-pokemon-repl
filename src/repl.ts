8097f33db20c95e0f5acf76c2bffaeb3b94d35bd
Nick Sikkema <contact@nicksikkema.com>
2025-08-15 20:07:48 -0500

    CH1L4 - complete

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { createInterface } from 'readline';
+
+
+export function startREPL() {
+    const rl = createInterface({ input: process.stdin, output: process.stdout, prompt: "Pokedex > " });
+
+    rl.prompt();
+
+    rl.on('line', (line: string) => {
+        let words = cleanInput(line);
+        if (words.length === 0) {
+            rl.prompt()
+            return;
+        }
+
+        const commandName = words[0]
+        console.log(`Your command was: ${commandName}`);
+        rl.prompt()
+    })
+
+}
+
 export function cleanInput(input: string): string[] {
     return input.toLowerCase().trim().split(" ").filter(Boolean);
 }
+
+
